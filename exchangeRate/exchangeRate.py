3aa4a29dd836be98a25b96a183dc511bff16cdcb
yuryshakhov <shakhov-yurko@yandex.ru>
2021-03-06 16:05:11 +0300

    code beautifier

--- exchangeRate/exchangeRate.py
+++ exchangeRate/exchangeRate.py
@@ -7,17 +7,19 @@ from zeep import Client
 from zeep.helpers import serialize_object
 
 
-def cbr(newCursDict):
+def cbr():
+    new_curs_dict = {}
     client = Client('https://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx?WSDL')
     service = client.bind('DailyInfo', 'DailyInfoSoap12')
-    todayCursDict = serialize_object(service.GetCursOnDate(date.today()))
+    today_curs_dict = serialize_object(service.GetCursOnDate(date.today()))
 
-    for i in range(0, len(todayCursDict['_value_1']['_value_1'])):
-        newValuteCurs = {
-            todayCursDict['_value_1']['_value_1'][i]['ValuteCursOnDate']['Vname'].strip():
-                todayCursDict['_value_1']['_value_1'][i]['ValuteCursOnDate']['Vcurs']
+    for i in range(0, len(today_curs_dict['_value_1']['_value_1'])):
+        new_valute_curs = {
+            today_curs_dict['_value_1']['_value_1'][i]['ValuteCursOnDate']['Vname'].strip():
+                today_curs_dict['_value_1']['_value_1'][i]['ValuteCursOnDate']['Vcurs']
         }
-        newCursDict.update(newValuteCurs)
+        new_curs_dict.update(new_valute_curs)
+    return new_curs_dict
 
 
 def morpher(n_val, unit_val):
@@ -34,20 +36,19 @@ def morpher(n_val, unit_val):
 
 
 if __name__ == "__main__":
-    valuteName = input('Enter valute name: ')
-    newCursDict = {}
-    cbr(newCursDict)
-    if valuteName not in newCursDict.keys():
+    valute_name = input('Enter valute name: ')
+    curs_dict = cbr()
+    if valute_name not in curs_dict.keys():
         print("This valute name does not exist. Choose it from the list:")
-        for item in newCursDict.keys():
+        for item in curs_dict.keys():
             print(item)
-        valuteName = input('Enter valute name: ')
+        valute_name = input('Enter valute name: ')
 
-    valuteCurs = newCursDict[valuteName]
-    penny = int(round(valuteCurs - floor(valuteCurs), 2)*100)
+    valute_curs = curs_dict[valute_name]
+    penny = int(round(valute_curs - floor(valute_curs), 2) * 100)
 
-    rubles_data = morpher(trunc(valuteCurs), "рубли")
+    rubles_data = morpher(trunc(valute_curs), "рубли")
     penny_data = morpher(penny, "копейки")
 
-    print('один', valuteName.lower(), 'равен', rubles_data.get('n').get('Д'), rubles_data.get('unit').get('Д'), penny,
+    print('один', valute_name.lower(), 'равен', rubles_data.get('n').get('Д'), rubles_data.get('unit').get('Д'), penny,
           penny_data.get('unit').get('Д'))
